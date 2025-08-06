a53600da5aed158e8022183ee5cc97fbd5be5d47
dj1530954213 <1530954213@qq.com>
2025-08-06 10:14:09 +0800

    删除无用的文件

--- 重构前代码/src/logging/mod.rs
+++
@@ -1,7 +0,0 @@
-//! 日志记录模块
-//!
-//! 提供结构化日志记录、日志轮转和敏感信息脱敏功能
-
-pub mod logger_config;
-
-pub use logger_config::*;
--- 重构前代码/src/monitoring/mod.rs
+++
@@ -1,7 +0,0 @@
-//! 系统监控模块
-//!
-//! 提供系统性能监控、业务指标监控和健康检查功能
-
-pub mod system_monitor;
-
-pub use system_monitor::*;
